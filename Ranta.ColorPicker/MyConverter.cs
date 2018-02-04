b4f95ccd7f07094ea6024ab6a9bfa8d9b51576c8
HarrisZhang <harriszhang@live.cn>
2018-02-04 11:16:43 +0800

    代码迁移

---
+++ Ranta.ColorPicker/MyConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Ranta.ColorPicker
+{
+    public class MyConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value.ToString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            try
+            {
+                int x = int.Parse(value.ToString());
+                if (x > 255)
+                    x = 255;
+                return x;
+            }
+            catch (Exception)
+            {
+                return 0;
+            }
+        }
+    }
+}
---
+++ Ranta.ColorPicker/RantaColor.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace Ranta.ColorPicker
+{
+    public class RantaColor : DependencyObject, INotifyPropertyChanged
+    {
+        public static DependencyProperty RedProperty = DependencyProperty.Register("Red", typeof(byte), typeof(RantaColor), new PropertyMetadata((byte)0, new PropertyChangedCallback(RedPropertyChanged)));
+
+        public static DependencyProperty GreenProperty = DependencyProperty.Register("Green", typeof(byte), typeof(RantaColor), new PropertyMetadata((byte)0, new PropertyChangedCallback(GreenPropertyChanged)));
+
+        public static DependencyProperty BlueProperty = DependencyProperty.Register("Blue", typeof(byte), typeof(RantaColor), new PropertyMetadata((byte)0, new PropertyChangedCallback(BluePropertyChanged)));
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public byte Red
+        {
+            get
+            {
+                return (byte)GetValue(RedProperty);
+            }
+            set
+            {
+                SetValue(RedProperty, value);
+            }
+        }
+
+        public byte Green
+        {
+            get
+            {
+                return (byte)GetValue(GreenProperty);
+            }
+            set
+            {
+                SetValue(GreenProperty, value);
+            }
+        }
+
+        public byte Blue
+        {
+            get
+            {
+                return (byte)GetValue(BlueProperty);
+            }
+            set
+            {
+                SetValue(BlueProperty, value);
+            }
+        }
+
+        public RantaColor()
+        {
+            color = Color.FromRgb(0, 0, 0);
+            brush = new SolidColorBrush(color);
+        }
+
+        private Color color;
+
+        private SolidColorBrush brush;
+
+        public Color Color
+        {
+            get
+            {
+                return color;
+            }
+        }
+
+        public SolidColorBrush Brush
+        {
+            get
+            {
+                return brush;
+            }
+        }
+
+        public static void RedPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            RantaColor rantaColor = (RantaColor)d;
+
+            rantaColor.color = Color.FromRgb(rantaColor.Red, rantaColor.Green, rantaColor.Blue);
+
+            rantaColor.brush = new SolidColorBrush(rantaColor.color);
+
+            rantaColor.OnColorChanged();
+        }
+        public static void GreenPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            RantaColor rantaColor = (RantaColor)d;
+
+            rantaColor.color = Color.FromRgb(rantaColor.Red, rantaColor.Green, rantaColor.Blue);
+
+            rantaColor.brush = new SolidColorBrush(rantaColor.color);
+
+            rantaColor.OnColorChanged();
+        }
+        public static void BluePropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            RantaColor rantaColor = (RantaColor)d;
+
+            rantaColor.color = Color.FromRgb(rantaColor.Red, rantaColor.Green, rantaColor.Blue);
+
+            rantaColor.brush = new SolidColorBrush(rantaColor.color);
+
+            rantaColor.OnColorChanged();
+        }
+
+        private void OnColorChanged()
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs("Color"));
+
+                PropertyChanged(this, new PropertyChangedEventArgs("Brush"));
+            }
+        }
+    }
+}
